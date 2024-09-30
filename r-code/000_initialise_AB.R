255b1cd915e57883c2eb32463c66a0d86485c267
ann-1302 <ann1302.benji@gmail.com>
2024-09-30 17:28:41 +1300

    initial commit

--- r-code/000_initialise_AB.R
+++ r-code/000_initialise_AB.R
@@ -1,26 +1,11 @@
-# Conditionally install packages
-if(!require(RPostgres)) {
-  install.packages("RPostgres")
-}
+library(readr)
+library(tidyverse)
 
-# Manually moved the files. Getting the files from the working directory 
-concord <- "./data/urban_rural_to_sa2_concord_2023.csv"
+#set date
+current_date <- format(Sys.Date(), "%Y%m%d")
+raw_output_file <- paste0("./data/", current_date, ".urban_rural_to_sa2_concord_2023.csv")
+# Read the CSV without treating any column as row names
+raw_data <- read.csv("./data/urban_rural_to_sa2_concord_2023.csv", header = TRUE, row.names = NULL)
 
-# Formatting 
-current_date <- '20240929'
 
-# Output filepaths (files will be created from this R script)
-concord_output_file <- paste0("./data/", current_date, ".urban_rural_to_sa2_concord_2023.csv")
-
-# Read the CSV file without setting row names
-concord_data <- read.csv(concord, row.names = NULL, stringsAsFactors = FALSE)
-
-# Save the file after reading
-write.csv(concord_data, concord_output_file, row.names = FALSE)
-
-# Check whether the output file exists or not
-concord_exists <- file.exists(concord_output_file)
-
-# Print message to show paths of the saved file
-cat("Files saved successfully:\n",
-    "New urban_rural_to_sa2_concord data csvfile: ", concord_output_file, "exists:", concord_exists, "\n")
+write.csv(raw_data, raw_output_file, row.names = FALSE)
