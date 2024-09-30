aa7499e5a3a971b150f0675e32c0e82d219e3ae9
Brayden Davies <braydendavies@Braydens-MacBook-Pro.local>
2024-09-30 15:48:08 +1300

    (brayden) update the file dat sa2_2023. better code

--- r-code/000_initialise_BD.R
+++ r-code/000_initialise_BD.R
@@ -3,6 +3,7 @@ library("tidyverse")
 library("dplyr")
 library("readr")
 library("arrow")
+library("tibble") 
 
 # conditionally install packages
 if(!require(RPostgres)) {
@@ -15,11 +16,11 @@ subnational_pop <- "./data/subnational_pop_ests.csv"
 
 # current date in YYYYMMDD format
 current_date <- format(Sys.Date(), "%Y%m%d")
-sa2_2023_output_file <- paste0(output_dir, current_date, ".clean-sa2_2023.csv")
-subnational_pop_output_file <- paste0(output_dir, current_date, ".clean-subnational_pop_ests.csv")
+sa2_2023_output_file <- paste0("./data/", current_date, ".clean-sa2_2023.csv")
+subnational_pop_output_file <- paste0("./data/", current_date, ".clean-subnational_pop_ests.csv")
 
 # Read the data
-sa2_2023_data <- read.csv(sa2_2023)
+sa2_2023_data <- read.csv(sa2_2023, row.names = 1)
 subnational_pop_data <- read.csv(subnational_pop)
 
 #removing some variables as they are duplicates
@@ -33,7 +34,10 @@ subnational_pop_clean <- subnational_pop_data %>%
   )
 
 # Remove the second column from sa2_2023_data since it's nothing
-sa2_2023_clean <- sa2_2023_data %>% select(-2)
+# Move row names into a new column called "Code" (you can rename as necessary)
+sa2_2023_clean <- sa2_2023_data %>%
+  rownames_to_column(var = "Code") %>%
+  select(-3)
 
 # Write the cleaned data to the output files
 write.csv(sa2_2023_clean, sa2_2023_output_file, row.names = FALSE)
@@ -47,3 +51,6 @@ subnational_pop_exists <- file.exists(subnational_pop_output_file)
 cat("Files saved successfully:\n",
     "New sa2_2023 data csvfile: ", sa2_2023_output_file, "exists:", sa2_2023_exists, "\n",
     "New subnational_pop data csvfile: ", subnational_pop_output_file, "exists:", subnational_pop_exists, "\n")
+
+view(sa2_2023_clean)
+view(subnational_pop_clean)
