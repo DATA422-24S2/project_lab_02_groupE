e5327abee2f5ce7128d9063f0b3a4a25558e46bb
david-ewing-nz <dew59@uclive.ac.nz>
2024-10-08 06:57:56 +1300

    oopse, I forgot the run.R script

---
+++ r-code/run.R
@@ -0,0 +1,19 @@
+# run.R 
+#
+# if the team wants a different name please change 
+# 
+# 
+#
+#
+# This file runs the final application
+#
+#
+
+#browser()
+source("./r-code/00_initialise.R")
+source("./r-code/01_import.R")
+source("./r-code/02_clean.R")
+#source("./r-code/03_analsye.R")
+#Source("./r-code/04_visulaise.R")
+#source("./r-code/04_export.R")
+
